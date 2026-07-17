3d4f6a52cea2ebfdaa9e748474157fc2d2bda54f
Dewith <dewithmiramon@gmail.com>
2026-07-17 14:38:04 -0500

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Dewith Andres Miramon Barrios on 17/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
